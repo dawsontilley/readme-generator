5e699c9fbb16849b3fb572159caf306c948dedc4
Dawson Tilley <16dit@queenus.ca>
2022-07-18 15:37:21 -0400

    second commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+const writeFile  = require('./utils/generate-site');
+
+const promptUser = readMeData => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'project-title',
+      message: 'What is your project title',
+    
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description for your project',
+
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Add installation instructions',
+      
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage instructions',
+      
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message:'provide contribution instructions',
+
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'provide testing instructions'
+
+    },
+    {
+      type:'checkbox',
+      name: 'license',
+      message:'select a liscense option',
+      choices:['MIT','GNU','Apache']
+
+
+    },
+    {
+      type:'input',
+      name: 'githubUsername',
+      message:'add a github username'
+    },
+    {
+      type: 'name',
+      name: 'emailAddress',
+      message: 'add an email address'
+    }
+
+
+  ]);
+};
+
+
+promptUser()
+  //.then(promptProject)
+  .then(readMeData => {
+    return generatePage(readMeData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ utils/generate-site.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+// writing files
+const writeFile = fileContent => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/readMe.md', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    });
+  });
+};
+
+// copying file
+
+
+module.exports = { writeFile };
